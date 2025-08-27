c4cfe99038ead1c3048b29c491595d8b3dfa0362
lbpereira09 <00001127879364sp@al.educacao.sp.gov.br>
2025-08-26 23:38:40 -0300

    Add files via upload

---
+++ Unidade 6 - Alura/script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoacc = document.getElementById('btn-acessibilidade');
+    const opacc = document.getElementById('opcoes-ac');
+    var img = document.querySelector("#image");
+
+    botaoacc.addEventListener('click', function () {
+        botaoacc.classList.toggle('rotacao-botao');
+        opacc.classList.toggle('apresenta-lista');
+    })
+
+    const FonteMaior = document.getElementById('aumentar-fonte');
+    const FonteMenor = document.getElementById('diminuir-fonte');
+
+    const mudacons = document.getElementById('alterna-contraste');
+
+    let tamanhoAtual = 1;
+
+    FonteMaior.addEventListener('click', function () {
+        tamanhoAtual += 0.1;
+        document.body.style.fontSize = `${tamanhoAtual}rem`
+    })
+    FonteMenor.addEventListener('click', function () {
+        tamanhoAtual -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtual}rem`
+    })
+    mudacons.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste');
+        const imgAtual = img.getAttribute('src');
+
+        if (imgAtual.includes('cbjr-logo2.png')) {
+            img.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/6/62/Cbjrlogo.png');
+        } else {
+            img.setAttribute('src', 'img/cbjr-logo2.png');
+        }
+    })
+})
+
+ScrollReveal().reveal('#inicio', { delay: 250 });
+ScrollReveal().reveal('#cbjr', { delay: 250 });
+ScrollReveal().reveal('#galeria', { delay: 250 });
+ScrollReveal().reveal('#contato', { delay: 250 });
